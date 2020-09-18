d62f8ff094078fc62335b174fa9b3d806efd5a65
tech234a <46801700+tech234a@users.noreply.github.com>
2020-09-18 14:50:09 -0400

    Updated DB Schema

--- dbfile.db.sql
+++ dbfile.db.sql
@@ -1,11 +1,4 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "workers" (
-	"WorkerID"	TEXT NOT NULL UNIQUE,
-	"CreatedTime"	TEXT NOT NULL,
-	"LastAliveTime"	TEXT NOT NULL,
-	"LastAliveIP"	TEXT NOT NULL,
-	PRIMARY KEY("WorkerID")
-);
 CREATE TABLE IF NOT EXISTS "main" (
 	"BatchID"	INTEGER NOT NULL,
 	"BatchStatus"	INTEGER NOT NULL,
@@ -18,18 +11,26 @@ CREATE TABLE IF NOT EXISTS "main" (
 	"BatchSize"	INTEGER,
 	PRIMARY KEY("BatchID" AUTOINCREMENT)
 );
+CREATE TABLE IF NOT EXISTS "workers" (
+	"WorkerID"	TEXT NOT NULL UNIQUE,
+	"CreatedTime"	TEXT NOT NULL,
+	"LastAliveTime"	TEXT NOT NULL,
+	"LastAliveIP"	TEXT NOT NULL,
+	"WorkerVersion"	INTEGER,
+	PRIMARY KEY("WorkerID")
+);
 CREATE INDEX IF NOT EXISTS "main_combined" ON "main" (
 	"BatchID",
 	"WorkerKey",
 	"RandomKey"
-);
-CREATE INDEX IF NOT EXISTS "worker_id" ON "workers" (
-	"WorkerID"
 );
 CREATE INDEX IF NOT EXISTS "main_BatchStatus" ON "main" (
 	"BatchStatus"
 );
 CREATE INDEX IF NOT EXISTS "main_BatchContent" ON "main" (
 	"BatchContent"
+);
+CREATE INDEX IF NOT EXISTS "worker_id" ON "workers" (
+	"WorkerID"
 );
 COMMIT;
